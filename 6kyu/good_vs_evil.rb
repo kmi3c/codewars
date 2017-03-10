d02066a7535c77dccd4befef590e8ec63ff7618e
padamski <pawel.adamski@connectmedica.com>
2017-03-10 10:36:52 +0100

    Remove unnecessary 'pry'

--- 6kyu/good_vs_evil.rb
+++ 6kyu/good_vs_evil.rb
@@ -50,7 +50,6 @@
 #  "Battle Result: Good triumphs over Evil" if good wins,
 #  "Battle Result: Evil eradicates all trace of Good" if evil wins, or
 #  "Battle Result: No victor on this battle field" if it ends in a tie.
-require 'pry'
 RESULTS = [
   'No victor on this battle field',
   'Good triumphs over Evil',
