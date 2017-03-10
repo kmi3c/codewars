49840aec2e742294feb1b1bbbc12970164d0d750
padamski <pawel.adamski@connectmedica.com>
2017-03-10 18:43:18 +0100

    Refactor to reduce LOC

--- 6kyu/calculating_with_functions.rb
+++ 6kyu/calculating_with_functions.rb
@@ -15,29 +15,13 @@
 #   The most outer function represents the left operand, the most inner function
 #   represents the right operand
 require 'pry'
-[
-  [:zero, 0],
-  [:one, 1],
-  [:two, 2],
-  [:three, 3],
-  [:four, 4],
-  [:five, 5],
-  [:six, 6],
-  [:seven, 7],
-  [:eight, 8],
-  [:nine, 9]
-
-].each do |method, value|
+%w(zero one two three four
+   five six seven eight nine).each_with_index do |method, value|
   define_method method do |*args|
     operation args, value
   end
 end
-[
-  [:plus, :+],
-  [:minus, :-],
-  [:times, :*],
-  [:divided_by, :/]
-].each do |method, value|
+%w(plus + minus - times * divided_by /).each_slice(2) do |method, value|
   define_method method do |*args|
     args << value
   end
