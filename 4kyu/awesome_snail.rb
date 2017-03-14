1e10eb793ee98ac18049273b34cba7e80bea93d1
padamski <pawel.adamski@connectmedica.com>
2017-03-14 15:09:38 +0100

    Add Awesome Snail solution

--- 4kyu/awesome_snail.rb
+++ 4kyu/awesome_snail.rb
@@ -1,3 +1,5 @@
+# Same Snail Sort solution but more awesome :)
+# Added just to know that is possible
 def awesome_snail(array)
   array.empty? ? [] : array.shift + awesome_snail(array.transpose.reverse)
 end
